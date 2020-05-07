e5b277d163df5bceed95319ec5b5437f6a42c689
arifpro <arif.swfu@outlook.com>
2020-05-07 12:21:53 +0800

    Test of post method in form

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ let express = require('express')
 
 let app = express()
 
+app.use(express.urlencoded({extended: false}))
+
 app.get('/', function(req, res) {
     res.send(`<!DOCTYPE html>
     <html>
@@ -16,9 +18,9 @@ app.get('/', function(req, res) {
         <h1 class="display-4 text-center py-1">To-Do App</h1>
         
         <div class="jumbotron p-3 shadow-sm">
-          <form>
+          <form action="/create-item" method="POST">
             <div class="d-flex align-items-center">
-              <input autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
@@ -54,6 +56,9 @@ app.get('/', function(req, res) {
     </html>`)
 })
 
-
+app.post('/create-item', function(req, res) {
+    console.log(req.body.item)
+    res.send('Thank for submitting to form')
+})
 
 app.listen(4000)
