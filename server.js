ea3d9e0a00bf63bd008cc3a416c039edecfc590f
arifpro <arif.swfu@outlook.com>
2020-05-08 19:23:12 +0800

    done

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (port == null  ||  port == '') {
 
 app.use(express.static('public'))
 
-let connectionString = process.env.DB_PATH
+let connectionString = 'mongodb+srv://arif:Arif2743@cluster0-io81u.mongodb.net/TodoApp?retryWrites=true&w=majority'
 let secondParameter = {useNewUrlParser: true, useUnifiedTopology: true}
 mongodb.connect(connectionString, secondParameter, function(err, client) {
   db = client.db()
